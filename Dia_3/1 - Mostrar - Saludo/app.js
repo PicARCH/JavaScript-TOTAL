54c12696f5d7abf321f7629a999ed839b8ee3725
Jimmy <JodiroCC962@gmail.com>
2023-12-21 16:38:46 -0400

    Se actualizo

---
+++ Dia_3/1 - Mostrar - Saludo/app.js
@@ -0,0 +1,19 @@
+// Declaración de una función llamada saludar
+function saludar(){
+     // Alerta que mostrará el mensaje "Hola Mundo" cuando la función sea llamada
+    alert("Hola Mundo");
+}
+/*
+    Explicación:
+
+    function saludar() {: Aquí se declara una función llamada saludar. 
+        Las funciones en JavaScript son bloques de código que se pueden
+         ejecutar cuando son llamados. En este caso, la función no toma ningún argumento.
+
+    alert("Hola Mundo");: Dentro del cuerpo de la función, hay una instrucción alert
+     que mostrará un cuadro de diálogo en el navegador con el mensaje "Hola Mundo".
+      La función alert es una función incorporada en JavaScript que se utiliza para
+       mostrar mensajes simples en el navegador.
+
+    }: Cierra el cuerpo de la función.
+*/
